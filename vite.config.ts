47f499835e93cea4f5ab6e65a0f83422327b9073
Rutuja Jamdade <rutujaj6589@gmail.com>
2026-08-03 12:11:15 +0530

    fix: vercel 404 asset paths

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/family-first-landing-page/',
   plugins: [
     react(),
     tailwindcss(),
